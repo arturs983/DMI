4fdda27c3489af6c2514ab928651c0daf9937d64
aijatikuma@inbox.lv <boks1234@inbox.lv>
2017-10-11 11:53:41 +0300

    201701011_11_50

--- shell-operators.sh
+++ shell-operators.sh
@@ -29,6 +29,7 @@ echo "2 % 3 ="$val25
 END
 
 #3.piemērs - operācijas ar mainīgajiem
+: <<'END'
 a=29
 b=15
 val31=`expr $a + $b`
@@ -41,6 +42,27 @@ val34=`expr $a / $b`
 echo "$a / $b ="$val34
 val35=`expr $a % $b`
 echo "$a % $b ="$val35
+END
+
+#4.piemērs - operācijas ar argumentiem
+if [ $# ==2 ]
+# ja (if) skriptam nodoto argumentu skaits ($#) ir vienāds (==) ar 2
+then
+# tad (then) izpildām sekojošās darbības līdz
+a=$1
+b=$2
+val41=`expr $a + $b`
+echo "$a + $b ="$val41
+val42=`expr $a - $b`
+echo "$a - $b ="$val42
+val43=`expr $a \* $b`
+echo "$a \ $b ="$val43
+val44=`expr $a / $b`
+echo "$a / $b ="$val44
+val45=`expr $a % $b`
+echo "$a % $b ="$val45
+# .... šai vietai
+fi
 
 a=10
 b=10
