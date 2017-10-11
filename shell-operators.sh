2526b3520e55ffae5a8867c66ea4eb06b5037d10
aijatikuma@inbox.lv <boks1234@inbox.lv>
2017-10-11 09:53:41 +0300

    201701011_09_50

---
+++ shell-operators.sh
@@ -0,0 +1,73 @@
+c#!/bin/sh
+
+#1.piemērs - izteiksmes pieraksts
+val1=`expr 2+2`
+echo "Rezultāts 2+2 (pēdiņas, bez atstarpēm)" $val1
+val2="expr 2 + 2"
+echo "Rezultāts 2+2 (pēdiņas,  ar atstarpēm)" $val2
+val3=`expr 2+2`
+echo "Rezultāts 2+2 (parasti apostrofi, bez atstarpēm)" $val3
+val4=`expr 2 + 2`
+echo "Rezultāts 2+2 (parasti apostrofi, a4r atstarpēm)" $val4
+val5=`expr 2+2`
+echo "Rezultāts 2+2 (neparasti apostrofi, bez atstarpēm)" $val5
+val6=`expr 2 + 2`
+echo "Rezultāts 2+2 (neparasti apostrofi, ar atstarpēm)" $val6
+
+#2.piemērs - operācijas ar konstantēm
+: <<'END'
+val21=`expr 2 + 3`
+echo "2 + 3 ="$val21
+val22=`expr 2 - 3`
+echo "2 - 3 ="$val22
+val23=`expr 2 \* 3`
+echo "2 \ 3 ="$val23
+val24=`expr 2 / 3`
+echo "2 / 3 ="$val24
+val25=`expr 2 % 3`
+echo "2 % 3 ="$val25 
+END
+
+#3.piemērs - operācijas ar mainīgajiem
+a=29
+b=15
+val31=`expr $a + $b`
+echo "$a + $b ="$val31
+val32=`expr $a - $b`
+echo "$a - $b ="$val32
+val33=`expr $a \* $b`
+echo "$a \ $b ="$val33
+val34=`expr $a / $b`
+echo "$a / $b ="$val34
+val35=`expr $a % $b`
+echo "$a % $b ="$val35
+
+a=10
+b=10
+
+val=`expr $a + $b`
+echo "a + b : $val"
+
+val=`expr $a - $b`
+echo "a - b : $val"
+
+val=`expr $a \* $b`
+echo "a * b : $val"
+
+val=`expr $b / $a`
+echo "b / a : $val"
+
+val=`expr $b % $a`
+echo "b % q : $val"
+
+if [ $a == $b ]
+then
+echo "a is equal to b"
+fi
+
+if [  $a != $b ]
+then 
+echo "a is not equal to b"
+fi
+
+
