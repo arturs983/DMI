03ae32c1f9844ce75ea40c1f2f13361ce8a98d99
aijatikuma@inbox.lv <boks1234@inbox.lv>
2017-11-08 09:48:46 +0200

    20171109_09_50

---
+++ PYTHON/hello.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+#dzejolis
+print 'oliņ \n\tboliņ \n\t\trimpiņ \n\t\t\tajiņ \n\t\tvējiņ \n\tbums \nbrīvs.'
+
+
+#7.piemērs
+print 'Here the \rcontent before this is removed.'
+
+
+#6.piemērs
+print 'Here \vthe \vspaces \vhave \vvertical \vtab \vspaces.'
+
+
+#5.piemērs
+print 'Here \tthe \tspaces \thave \thorizontal \ttab \tspaces.'
+
+#4.piemērs
+print 'Here \nthe \nspaces \nare \nnewlined.'
+
+#3.piemērs
+print "Here \bthe \bspaces \bare \bbackspaced."
+
+'''
+#1.piemērs
+print "Hello World!"
+'''
---
+++ PYTHON/variables.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+#a=10
+#echo $a
+#echo "Mainīgā a vērtība ir : $a"
+#a=65
+a='A'
+print ("Mainīgā a vērtība kā decimāls skaitlis: %d"%(a))
+print ("Mainīgā a vērtība kā heksadecimāls skaitlis: %x"%(a))
+print ("Mainīgā a vērtība kā oktāls skaitlis: %o"%(a))
+print ("Mainīgā a vērtība kā simbols: %c"%(a))
