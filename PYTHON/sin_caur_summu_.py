73ff7992ca14b01f8813c5b308f9ea5a4df76ce0
aijatikuma@inbox.lv <boks1234@inbox.lv>
2018-01-23 14:29:24 +0200

    23012018_14_30

--- PYTHON/sin_caur_summu_.py
+++ PYTHON/sin_caur_summu_.py
@@ -1,24 +1,33 @@
-
-# -*- coding: utf-8 -*-
 from math import sin
 
 def mans_sinuss(x):
     k = 0
     a = (-1)**0*x**1/(1)
     S = a
-    print "Izdruka no lietotāj funkcijas a0 = %6.2f S0 = %.2f"%(a,S)
-    while k < 3:
+    a1 = (-1)**1*x**1/6
+    S1 = a1 + a
+    print "a%d = %6.2f S%d = %6.2f"%(k,a,k,S)
+    print "a%d = %6.2f S%d = %6.2f"%(k+1,a1,k+1,S1)
+    arra=[]
+    arrS=[]
+
+    while k < 500:
         k = k + 1
         R = (-1) * x**2/((2*k)*(2*k+1))
         a = a * R
-        S = S + a
-        print "a%d = %6.2f S%d = %6.2f"%(k,a,k,S)
-    print "Izdruka no galvenas funkcija Beigas!"
-    return S 
+        S=S+a
+        #print "a%d = %6.2f S%d = %6.2f"%(k,a,k,S)
+        arra.append(a)
+        arrS.append(S)
 
+    print "a%d = %6.2f S%d = %6.2f"%(k-1,arra[-2],k-1,arrS[-2])
+    print "a%d = %6.2f S%d = %6.2f"%(k,a,k,S)
+    #print "Beigas!"
+    return S
 
-x = 1. * input("Izdruka no galvenas funkcijas Lietotāj, lūdzu, ievadi  argumentu (x): ")
-y = sin(x)
-print "Izdruka no galvenas funkcijas standarta sin(%.2f)=%.2f"%(x,y)
-yy = mans_sinuss(x) #funkcijas izpilde un piešķiršana
-print "Izdruka no galvenas funkcijas mans sin(%.2f)=%.2f"%(x,yy)
+x=1. * input("Lietotaj, ludzu, ievadi x argumentu: ")
+y=sin(x)
+print "Standarta sin(%.2f) = %.2f"%(x,y)
+yy = mans_sinuss(x)
+print "Mans sin(%.2f) = %.2f"%(x,yy)
+
